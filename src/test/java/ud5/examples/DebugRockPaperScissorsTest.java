a7bf61fdd9b3586fcefe755781b54fbe3e4340b3
joapuiib <joapuiib@gmail.com>
2024-01-15 16:59:29 +0100

    ud5: rock paper scissors tests

--- src/test/java/ud5/examples/DebugRockPaperScissorsTest.java
+++ src/test/java/ud5/examples/DebugRockPaperScissorsTest.java
@@ -7,9 +7,51 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DebugRockPaperScissorsTest {
     @Test
+    @DisplayName("Empat: Pedra vs Pedra")
+    void pedraEmpatPedra() {
+        assertEquals(0, DebugRockPaperScissors.guanya("pedra", "pedra"));
+    }
+
+    @Test
+    @DisplayName("Perd: Pedra vs Paper")
+    void pedraPerdPaper() {
+        assertEquals(2, DebugRockPaperScissors.guanya("pedra", "paper"));
+    }
+
+    @Test
     @DisplayName("Guanya: Pedra vs Tisores")
     void pedraGuanyaTisores(){
         assertEquals(1, DebugRockPaperScissors.guanya("pedra", "tisores"));
+    }
+
+    @Test
+    @DisplayName("Guanya: Paper vs Pedra")
+    void paperGuanyaPedra() {
+        assertEquals(1, DebugRockPaperScissors.guanya("paper", "pedra"));
+    }
+
+    @Test
+    @DisplayName("Empat: Paper vs Paper")
+    void paperEmpatPaper() {
+        assertEquals(0, DebugRockPaperScissors.guanya("paper", "paper"));
+    }
+
+    @Test
+    @DisplayName("Perd: Paper vs Tisores")
+    void paperPerdTisores(){
+        assertEquals(2, DebugRockPaperScissors.guanya("paper", "tisores"));
+    }
+
+    @Test
+    @DisplayName("Perd: Tisores vs Pedra")
+    void tisoresPerdPedra() {
+        assertEquals(2, DebugRockPaperScissors.guanya("tisores", "pedra"));
+    }
+
+    @Test
+    @DisplayName("Guanya: Tisores vs Paper")
+    void tisoresGuanyaPaper() {
+        assertEquals(1, DebugRockPaperScissors.guanya("tisores", "paper"));
     }
 
     @Test
